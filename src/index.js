da2645a2439741b2ef70e7a5111729ea2659a783
sindhuvadla <113965681+sindhuvadla@users.noreply.github.com>
2022-10-23 07:04:38 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Topbar from './style/Topbar';
+//import Topbar from './style/Topbar';
+import Userprofile from './Userprofile';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Topbar />
+    <Userprofile />
   </React.StrictMode>
 );
 
